d1c265bde98288d0702f4a41d892f54b842921f4
Kilian McMahon <kil@kilmc.com>
2019-01-09 21:49:10 -0500

    Adjust colors and remove old aspect ratio

--- src/plugins/value/colors.js
+++ src/plugins/value/colors.js
@@ -13,14 +13,17 @@ const colorPlugin = {
   values: {
     black: '#000000',
     white: '#ffffff',
-    pink: '#ff9dd8'
+    pink: '#ff9dd8',
+    'totoro-belly': '#e1d6a8'
   },
+  sampleValues: ['black', 'pink'],
   valueModifiers: [
     {
       name: 'opacity',
       separator: '_',
       indicator: '\\d+',
-      modifierFn: hexToRgba
+      modifierFn: hexToRgba,
+      sampleIndicatorValues: ['01', '99', '50']
     }
   ]
 };
