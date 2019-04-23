f66427a1167ada945ce94334571f5f96f5b21b92
Keith Holjencin <keith.holjencin@gmail.com>
2019-04-23 15:11:17 -0400

    Add justify-content: space-evenly.

--- src/props/layout/flexbox/justify-content.js
+++ src/props/layout/flexbox/justify-content.js
@@ -1,17 +1,18 @@
 const justifyContent = {
-  propName: 'justify',
-  prop: 'justify-content',
+  propName: "justify",
+  prop: "justify-content",
   keywordValues: {
-    separator: '-',
+    separator: "-",
     values: {
-      'center': 'center',
-      'end': 'flex-end',
-      'start': 'flex-start',
-      'between': 'space-between',
-      'around': 'space-around'
+      center: "center",
+      end: "flex-end",
+      start: "flex-start",
+      between: "space-between",
+      around: "space-around",
+      evenly: "space-evenly"
     }
   },
-  propGroup: 'Flexbox'
+  propGroup: "Flexbox"
 };
 
 export default justifyContent;
